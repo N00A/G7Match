bd1bc18c029eaa0ffa2521f160efb4f7ae325a49
JohnSP <johnj262000@hotmail.com>
2025-08-17 21:38:35 -0500

    #Add: Primer commit

---
+++ rdg7/rdg7/src/main/java/com/G7Match/rdg7/Rdg7Application.java
@@ -0,0 +1,13 @@
+package com.G7Match.rdg7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Rdg7Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Rdg7Application.class, args);
+	}
+
+}
---
+++ rdg7/rdg7/src/test/java/com/G7Match/rdg7/Rdg7ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.G7Match.rdg7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Rdg7ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
