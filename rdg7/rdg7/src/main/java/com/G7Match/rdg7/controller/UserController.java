eb7a7d0cd6c0f9ecbb9c3ce5bb985cea20929c65
John Soto <johnjs@arqs.co>
2025-08-19 21:37:27 -0500

    #Add: user management functionality with CRUD operations and Swagger integration

--- rdg7/rdg7/src/main/java/com/G7Match/rdg7/controller/UserController.java
+++ rdg7/rdg7/src/main/java/com/G7Match/rdg7/controller/UserController.java
@@ -3,20 +3,42 @@ package com.G7Match.rdg7.controller;
 
 import com.G7Match.rdg7.model.UserModel;
 import com.G7Match.rdg7.services.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/user")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class UserController {
 
-    private UserService userService;
+    private final UserService userService;
 
     @GetMapping
     public UserModel getById(Long id){
         return userService.findById(id);
+    }
+
+    @GetMapping("/all")
+    public List<UserModel> getAllUsers() {
+        return userService.getAllClubs();
+    }
+
+    @PostMapping("/create")
+    public UserModel createUser(UserModel userModel) {
+        return userService.save(userModel);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public void deleteUser(@PathVariable Long id) {
+        userService.deleteById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public UserModel updateUser(@PathVariable Long id,@RequestBody UserModel userModel) {
+        return userService.updateUser(id, userModel);
     }
 
 }
