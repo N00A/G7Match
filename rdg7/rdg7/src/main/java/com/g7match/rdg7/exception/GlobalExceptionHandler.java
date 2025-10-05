ae595cac8ba75d2c2f647951cadab5e0ce7b4d1f
JohnSP <johnj262000@hotmail.com>
2025-10-04 21:23:18 -0500

    Se mejora manejo de errores y se repara swagger

--- rdg7/rdg7/src/main/java/com/g7match/rdg7/exception/GlobalExceptionHandler.java
+++ rdg7/rdg7/src/main/java/com/g7match/rdg7/exception/GlobalExceptionHandler.java
@@ -27,7 +27,7 @@ public class GlobalExceptionHandler {
     }
     @ExceptionHandler(Exception.class)
     public ResponseEntity<ApiResponse<Object>> handleGeneric(Exception ex) {
-        ApiResponse<Object> response = new ApiResponse<>(false, "Error interno del servidor", null);
+        ApiResponse<Object> response = new ApiResponse<>(false, "Error interno del servidor", ex.getMessage());
         return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(response);
     }
 }
